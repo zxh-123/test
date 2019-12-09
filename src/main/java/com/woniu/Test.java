c3b9fe5addbd876f26d7917a2d57d2a8048dedde
yuzhipeng <331663051@qq.com>
2019-12-09 21:57:04 +0800

    添加id

--- src/main/java/com/woniu/Test.java
+++ src/main/java/com/woniu/Test.java
@@ -4,4 +4,15 @@ import java.io.Serializable;
 
 public class Test implements Serializable{
 
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
 }
