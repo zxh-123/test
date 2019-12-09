743d15dea8e457f7d75fc959438418f25399e8c9
zxh <zxh@163.com>
2019-12-09 22:00:18 +0800

    1212323

--- src/main/java/com/woniu/Test.java
+++ src/main/java/com/woniu/Test.java
@@ -3,5 +3,28 @@ package com.woniu;
 import java.io.Serializable;
 
 public class Test implements Serializable{
-
+   private Integer id;
+   private String deptName;
+public Test(Integer id, String deptName) {
+	super();
+	this.id = id;
+	this.deptName = deptName;
+}
+public Integer getId() {
+	return id;
+}
+public void setId(Integer id) {
+	this.id = id;
+}
+public String getDeptName() {
+	return deptName;
+}
+public void setDeptName(String deptName) {
+	this.deptName = deptName;
+}
+@Override
+public String toString() {
+	return "Test [id=" + id + ", deptName=" + deptName + "]";
+}
+   
 }
